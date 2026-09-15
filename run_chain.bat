061571da94841fde08b153d3d5e750416fe38d8e
Adieltheanalyst <adielngugi@gmail.com>
2026-09-15 16:43:22 +0300

    new commit

--- run_chain.bat
+++ run_chain.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d C:\Users\Admin\Desktop\project_004
+cd /d "%~dp0"
 call .venv\Scripts\activate.bat
 if not exist logs mkdir logs
 python -m src.ingest.chain_snapshot >> logs\chain.log 2>&1
